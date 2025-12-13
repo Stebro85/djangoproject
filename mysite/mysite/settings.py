182850bb3ed6074156d054af36d31321e13a652f
Stebro85 <steven.broucke@outlook.be>
2025-12-13 17:24:50 +0100

    change debug

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xl5dx5a)41m@5vg9zgvp(cogfc77^3)bz22wv4!sve39b%49&x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
