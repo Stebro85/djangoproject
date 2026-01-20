243f2d105b086d94ecdbd4986360cf356734c7ca
Stebro85 <steven.broucke@outlook.be>
2026-01-20 11:23:28 +0100

    changes  forms

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(
@@ -14,5 +16,18 @@ def post_detail(request, pk):
     return render(request, 'blog/post_inhoud.html', 
 {'post': post})
 
+def post_nieuw(request):
+    if request.method == "POST":
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save() 
+            return redirect('post_inhoud', pk=post.pk)
+        else: 
+            form=PostForm()
+        return render(request, 'blog/post_edit.html', {'form': 
+form})
 
 # Create your views here.
