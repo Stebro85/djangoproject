c400dc9c5e0cdcdd21904b3e7b444ecd39c46eeb
Stebro85 <steven.broucke@outlook.be>
2026-01-20 10:18:32 +0100

    changes post_inhoud

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
@@ -7,6 +8,11 @@ def post_list(request):
         published_date__lte=timezone.now()
         ).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_inhoud.html', 
+{'post': post})
 
 
 # Create your views here.
